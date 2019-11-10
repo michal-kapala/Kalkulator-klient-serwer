1f8b019dce5d53e7ea70ccfc8318088f5d11850f
Jakub Chodubski <jakubchodubski@gmail.com>
2019-11-10 15:00:14 +0100

    switch - serwer

--- Serwer TCP/Serwer TCP/Serwer TCP.cpp
+++ Serwer TCP/Serwer TCP/Serwer TCP.cpp
@@ -2,8 +2,69 @@
 #include "header masks.hpp"
 #include <iostream>
 #include <windows.h>
+#include <math.h>
 #include <SFML/Network.hpp>//linkowanie dynamiczne plikow .dll
 using namespace sf;
+
+void floatTo2Int(float src, Uint64& dest1, Uint64& dest2) {
+	dest1 = src;
+	src -= dest1;
+	src *= 1000;
+	dest2 = src;
+}
+
+float root(Uint64 arg1, Uint64 arg2) {
+	float index = 1.0 / (float)arg2;
+	float ret = pow(arg1, index);
+	std::cout << "Operacja pierwiastkowania: (" << arg2 << ")th root of " << arg1 << " = " << ret << "\n";
+	return ret;
+}
+
+Uint64 power(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret = pow(arg1, arg2);
+	std::cout << "Operacja potegoawnia: " << arg1 << "^" << arg2 << " = " << ret << "\n";
+	return ret;
+}
+
+Uint64 lesserEqual(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret;
+	if (arg1 <= arg2) ret = 1;
+	else ret = 0;
+	std::cout << "Operacja <=: " << arg1 << "<=" << arg2 << "Odpowiedz: " << ret << "\n";
+	return ret;
+}
+
+Uint64 greaterEqual(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret;
+	if (arg1 >= arg2) ret = 1;
+	else ret = 0;
+	std::cout << "Operacja >=: " << arg1 << ">=" << arg2 << "Odpowiedz: " << ret << "\n";
+	return ret;
+}
+
+Uint64 add(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret = arg1 + arg2;
+	std::cout << "Operacja dodawania: " << arg1 << " + " << arg2 << " = "<< ret << "\n";
+	return ret;
+}
+
+Int64 subtract(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret = arg1 - arg2;
+	std::cout << "Operacja dodawania: " << arg1 << " - " << arg2 << " = " << ret << "\n";
+	return ret;
+}
+
+Uint64 multiply(Uint64 arg1, Uint64 arg2) {
+	Uint64 ret = arg1 * arg2;
+	std::cout << "Operacja dodawania: " << arg1 << " * " << arg2 << " = " << ret << "\n";
+	return ret;
+}
+
+float divide(Uint64 arg1, Uint64 arg2) {
+	float ret = arg1 / arg2;
+	std::cout << "Operacja dodawania: " << arg1 << " / " << arg2 << " = " << ret << "\n";
+	return ret;
+}
 
 void dispatchMessage(const Uint64 &msg, header &header)//wczytuje dane z wiadomosci do obiektu
 {
@@ -35,6 +96,58 @@ void serverProcess(const Uint64 messg[])
 	}
 
 	std::cout << "\nStatus: " << header.statusID << "\nDlugosc danych: " << header.datalength << "\nArgumentow: " << header.secparam + 1 << "\nID sesji: " << header.sessionID << std::endl;
+
+
+	Uint64 res1, res2;
+	switch (header.operationID) {
+	case 0: { //dodawanie
+		header.secparam = 0;
+		res1 = add(messg[1], messg[2]);
+		break;
+	}
+	case 1: { //odejmowanie
+		header.secparam = 0;
+		res1 = subtract(messg[1], messg[2]);
+		break;
+	}
+	case 2: { //mnożenie
+		header.secparam = 0;
+		res1 = multiply(messg[1], messg[2]);
+		break;
+	}
+	case 3: { //dzielenie
+		header.secparam = 1;
+		float resoult = divide(messg[1], messg[2]);
+		floatTo2Int(resoult, res1, res2); ///poprawowac nad minusem
+		break;
+	}
+	case 4: { // >=
+		header.secparam = 0;
+		res1 = greaterEqual(messg[1], messg[2]);
+		break;
+	}
+	case 5: { // <=
+		header.secparam = 0;
+		res1 = lesserEqual(messg[1], messg[2]);
+		break;
+	}
+	case 6: { // potega (podstawa, wykładnik)
+		res1 = power(messg[1], messg[2]);
+		header.secparam = 0;
+		break;
+	}
+	case 7: { // pierwiaste (podstawa, stopien)
+		header.secparam = 1;
+		float resoult = divide(messg[1], messg[2]);
+		floatTo2Int(resoult, res1, res2);
+		break;
+	}
+	case 8: { //silnia
+		break;
+	}
+	}
+
+	
 }
 
 int main()
